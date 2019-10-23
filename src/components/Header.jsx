3d159251e6cf433de31055ad6ce56335a148f6bb
alex_volkov <aleksandr.volkov@quardex.com>
2019-10-23 12:43:03 +0300

    test

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className="appHeader">
+            <img
+                src="https://www.designfreelogoonline.com/wp-content/uploads/2016/03/00106-3D-company-logo-design-free-logo-online-Template-03.png"
+                alt="site logo"/>
+            <span>Header</span>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className="mainContent">
+            <div>
+                <img src="https://picsum.photos/id/506/1800/500" alt=""/>
+            </div>
+            <div className="userInfo">
+                <img src="https://picsum.photos/id/447/100/100" alt=""/>
+            </div>
+            Content
+
+        </div>
+    );
+}
+
+export default Profile;
