fa3e6253b6d8ad5691829bc46efdaca2dfe938ba
borodaryda <gromixaka@gmail.com>
2020-03-18 17:32:45 +0200

    49 userPage finish

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,16 +3,31 @@ import './Users.css'
 
 const UsersContainer = (props) => {
 
+
+    if (props.users.length === 0) {
+        props.setUsers(
+            [
+                {id: 1, followed: true, fullName: 'Alex', status: 'Front And', location: {city:'Kharkiv', country: 'Ukraine'}, userIcon: 'https://picsum.photos/60/60'},
+                {id: 2, followed: false, fullName: 'Chernov', status: 'Full Stack', location: {city:'Kuiv', country: 'Ukraine'}, userIcon: 'https://picsum.photos/60/60'},
+                {id: 3, followed: true, fullName: 'Lera', status: 'Sells', location: {city:'Symu', country: 'Ukraine'}, userIcon: 'https://picsum.photos/60/60'},
+                {id: 4, followed: true, fullName: 'Slavon', status: 'Front And', location: {city:'Krakov', country: 'Poland'}, userIcon: 'https://picsum.photos/60/60'},
+                {id: 5, followed: false, fullName: 'Dima', status: 'React samurai', location: {city:'Minsk', country: 'Belarus'}, userIcon: 'https://picsum.photos/60/60'}
+            ]
+        )
+    }
+
+
     return  (
         <div>
             {
                 props.users.map( u => <div className="user-item" key={u.id}>
-                    <span>
+                    <span className="user-control">
                         <div className="user-icon_wrap"><img src={u.userIcon} alt={u.userIcon}/></div>
                         <div>
-                            <button>
-                                Follow
-                            </button>
+                            { u.followed
+                                ? <button onClick={ () => {props.unfollow(u.id)} }>Unfollow</button>
+                                : <button onClick={ () => {props.follow(u.id)} }>Follow</button>
+                            }
                         </div>
                     </span>
                     <span className="user-info">
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,11 +4,11 @@ const SET_USERS = "SET-USERS";
 
 let initialState = {
     users: [
-        {id: 1, followed: true, fullName: 'Alex', status: 'Front And', location: {city:'Kharkiv', country: 'Ukraine'}, userIcon: 'https://picsum.photos/60/60'},
-        {id: 2, followed: false, fullName: 'Chernov', status: 'Full Stack', location: {city:'Kuiv', country: 'Ukraine'}, userIcon: 'https://picsum.photos/60/60'},
-        {id: 3, followed: true, fullName: 'Lera', status: 'Sells', location: {city:'Symu', country: 'Ukraine'}, userIcon: 'https://picsum.photos/60/60'},
-        {id: 4, followed: true, fullName: 'Slavon', status: 'Front And', location: {city:'Krakov', country: 'Poland'}, userIcon: 'https://picsum.photos/60/60'},
-        {id: 5, followed: false, fullName: 'Dima', status: 'React samurai', location: {city:'Minsk', country: 'Belarus'}, userIcon: 'https://picsum.photos/60/60'}
+        // {id: 1, followed: true, fullName: 'Alex', status: 'Front And', location: {city:'Kharkiv', country: 'Ukraine'}, userIcon: 'https://picsum.photos/60/60'},
+        // {id: 2, followed: false, fullName: 'Chernov', status: 'Full Stack', location: {city:'Kuiv', country: 'Ukraine'}, userIcon: 'https://picsum.photos/60/60'},
+        // {id: 3, followed: true, fullName: 'Lera', status: 'Sells', location: {city:'Symu', country: 'Ukraine'}, userIcon: 'https://picsum.photos/60/60'},
+        // {id: 4, followed: true, fullName: 'Slavon', status: 'Front And', location: {city:'Krakov', country: 'Poland'}, userIcon: 'https://picsum.photos/60/60'},
+        // {id: 5, followed: false, fullName: 'Dima', status: 'React samurai', location: {city:'Minsk', country: 'Belarus'}, userIcon: 'https://picsum.photos/60/60'}
     ]
 }
 
