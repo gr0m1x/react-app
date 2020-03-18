2097a50229a2d6126bcf8b26d3a9997a55e9b810
borodaryda <gromixaka@gmail.com>
2020-03-18 19:21:46 +0200

    51 less get

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,28 +1,22 @@
 import React from "react";
 import './Users.css'
+import * as axios from "axios";
 
 const UsersContainer = (props) => {
 
-
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                {id: 1, followed: true, fullName: 'Alex', status: 'Front And', location: {city:'Kharkiv', country: 'Ukraine'}, userIcon: 'https://picsum.photos/60/60'},
-                {id: 2, followed: false, fullName: 'Chernov', status: 'Full Stack', location: {city:'Kuiv', country: 'Ukraine'}, userIcon: 'https://picsum.photos/60/60'},
-                {id: 3, followed: true, fullName: 'Lera', status: 'Sells', location: {city:'Symu', country: 'Ukraine'}, userIcon: 'https://picsum.photos/60/60'},
-                {id: 4, followed: true, fullName: 'Slavon', status: 'Front And', location: {city:'Krakov', country: 'Poland'}, userIcon: 'https://picsum.photos/60/60'},
-                {id: 5, followed: false, fullName: 'Dima', status: 'React samurai', location: {city:'Minsk', country: 'Belarus'}, userIcon: 'https://picsum.photos/60/60'}
-            ]
-        )
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            // debugger;
+            props.setUsers(response.data.items)
+        });
     }
-
 
     return  (
         <div>
             {
                 props.users.map( u => <div className="user-item" key={u.id}>
                     <span className="user-control">
-                        <div className="user-icon_wrap"><img src={u.userIcon} alt={u.userIcon}/></div>
+                        <div className="user-icon_wrap"><img src={u.photos.small != null ? u.photos.small : "https://picsum.photos/60/60"} alt={u.photos.small}/></div>
                         <div>
                             { u.followed
                                 ? <button onClick={ () => {props.unfollow(u.id)} }>Unfollow</button>
@@ -32,12 +26,12 @@ const UsersContainer = (props) => {
                     </span>
                     <span className="user-info">
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </span>
                     </span>
                 </div>)
