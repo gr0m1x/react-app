714ee1dd03e300a06974e4e7e5ade61cb2879425
gr0m1x <gromixaka@gmail.com>
2020-03-19 15:40:10 +0200

    53 less Class Component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,29 +2,33 @@ import React from "react";
 import './Users.css'
 import * as axios from "axios";
 
-const UsersContainer = (props) => {
+class Users extends React.Component {
 
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            // debugger;
-            props.setUsers(response.data.items)
-        });
+     getUsers = () => {
+        if (this.props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                // debugger;
+                this.props.setUsers(response.data.items)
+            });
+        }
     }
 
-    return  (
-        <div>
-            {
-                props.users.map( u => <div className="user-item" key={u.id}>
+    render() {
+        return  (
+            <div>
+                <button onClick={this.getUsers}>Get Users</button>
+                {
+                    this.props.users.map( u => <div className="user-item" key={u.id}>
                     <span className="user-control">
                         <div className="user-icon_wrap"><img src={u.photos.small != null ? u.photos.small : "https://picsum.photos/60/60"} alt={u.photos.small}/></div>
                         <div>
                             { u.followed
-                                ? <button onClick={ () => {props.unfollow(u.id)} }>Unfollow</button>
-                                : <button onClick={ () => {props.follow(u.id)} }>Follow</button>
+                                ? <button onClick={ () => {this.props.unfollow(u.id)} }>Unfollow</button>
+                                : <button onClick={ () => {this.props.follow(u.id)} }>Follow</button>
                             }
                         </div>
                     </span>
-                    <span className="user-info">
+                        <span className="user-info">
                         <span>
                             <div>{u.name}</div>
                             <div>{u.status}</div>
@@ -34,11 +38,11 @@ const UsersContainer = (props) => {
                             <div>{"u.location.city"}</div>
                         </span>
                     </span>
-                </div>)
-            }
-        </div>
-    );
+                    </div>)
+                }
+            </div>
+        );
+    };
+}
 
-};
-
-export default UsersContainer;
+export default Users;
