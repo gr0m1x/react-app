b70eff132a4f22f4cf7bfd7d99d66bf01040ae59
borodaryda <gromixaka@gmail.com>
2020-04-17 14:39:51 +0300

    Updates

---
+++ precache-manifest.c6718ba59ff7f3df97e89a9c2d44bf69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ddeff146fccee165a2cc2c2ae06326",
+    "url": "/social-react/index.html"
+  },
+  {
+    "revision": "270182e79b96d0dfc55f",
+    "url": "/social-react/static/css/3.881e1ba6.chunk.css"
+  },
+  {
+    "revision": "3828c0db3d603905485a",
+    "url": "/social-react/static/css/main.701a3f08.chunk.css"
+  },
+  {
+    "revision": "7a63c4529bc0fb12a7df",
+    "url": "/social-react/static/js/2.8335df0c.chunk.js"
+  },
+  {
+    "revision": "270182e79b96d0dfc55f",
+    "url": "/social-react/static/js/3.07481142.chunk.js"
+  },
+  {
+    "revision": "3828c0db3d603905485a",
+    "url": "/social-react/static/js/main.1a7e914b.chunk.js"
+  },
+  {
+    "revision": "1be8abfa1bb5f80441e3",
+    "url": "/social-react/static/js/runtime-main.b38fdca5.js"
+  }
+]);
